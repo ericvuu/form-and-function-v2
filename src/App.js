764f5f3f811ccc53fb3685c1c50677b2fb0d75be
Eric Vuu <ericlv91@gmail.com>
2021-05-24 13:00:54 -0700

    add header and nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "./Components/Header";
 import "./Styles/App.scss";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header/>
     </div>
   );
 }
