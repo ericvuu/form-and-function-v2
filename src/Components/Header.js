0ba289a8e05db4d91c4c33b3f5f193614865d31f
Eric Vuu <ericlv91@gmail.com>
2021-05-30 09:53:03 -0700

    added navigation animations

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import {NavLink, withRouter} from "react-router-dom";
+import {ReactComponent as UpArrow} from "../Assets/up-arrow-circle.svg";
+import { openMenu, closeMenu  } from "../Animations/menuAnimations";
 
-function Header() {
+import gsap from "gsap";
+
+let tl = gsap.timeline();
+
+const Header = ({history, dimensions}) => {
+
+  const[menuState, setMenuState] = useState ({menuOpened: false});
+
+  useEffect(() => {
+    //Listening for page changes.
+    history.listen(() => {
+      setMenuState({ menuOpened: false });
+    });
+    if (menuState.menuOpened === true) {
+      openMenu(dimensions.width);
+    } else if (menuState.menuOpened === false) {
+      closeMenu();
+    }
+  });
+
   return (
     <div className="header">
       <div className="container">
@@ -8,15 +30,20 @@ function Header() {
           <div className="logo">
             <a href="/">Form & Function</a>
           </div>
-          <div className="nav">
-            <span></span>
-            <span></span>
-            <span></span>
+          <div className="nav-toggle">
+            <div onClick={() => setMenuState({menuOpened: true}) } className="hamburger-menu">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div onClick={() => setMenuState({menuOpened: false}) } className="hamburger-menu-close">
+              <UpArrow/>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default Header
+export default withRouter(Header);
